772f139dc11900329db0140daf00ad052ee0236d
Rogério Neves dos Santos <103578702+RogerNSantos@users.noreply.github.com>
2024-01-18 17:39:12 -0300

    Add files via upload

--- classe/DesafioData.java
+++ classe/DesafioData.java
@@ -4,7 +4,12 @@ public class DesafioData {
 	
 	//CRIANDO ATRIBUTOS 
 		int dia;
-		String mes;
+		int mes;
 		int ano;
+		
+		String obterDataFormatada() {
+			return String.format("%d/%s/%d", dia, mes, ano);
+		}
+		
 
 }
--- classe/DesafioDataTeste.java
+++ classe/DesafioDataTeste.java
@@ -4,17 +4,15 @@ public class DesafioDataTeste {
 
 	public static void main(String[] args) {
 
-		//CRIANDO MÉTODOS (INSTÂNCIAS)
-		DesafioData calendario = new DesafioData ();
-		 calendario.dia = 23;
-		 calendario.mes = "Outubro";
-		 calendario.ano = 1986;
-		 
-		 System.out.println("Meu aniversário é " + calendario.dia + " de " 
-		 + calendario.mes + " de " + calendario.ano);
-		
-		 //OUTRA FORMA SIMPLIFICADA
-		 System.out.printf(" Meu aniversário é %d/%s/%d", calendario.dia, calendario.mes, calendario.ano);
+		// CRIANDO MÉTODOS (INSTÂNCIAS)
+		DesafioData calend = new DesafioData();
+		calend.dia = 23;
+		calend.mes = 10;
+		calend.ano = 1986;
+
+		String dataFormatada = calend.obterDataFormatada();
+		System.out.println(calend.obterDataFormatada());
+		System.out.printf("Meu aniversário é:" + dataFormatada);
 	}
 
 }
